abc691e757745b8a6f89d1433878f51783b70293
tramvaijsorg <d.oleg.work@gmail.com>
2024-11-29 10:53:53 +0000

    Release v5.9.2
    Co-authored-by: o.drapeza <o.drapeza@tinkoff.ru>
    GitOrigin-RevId: c3991714c38733d9d04d83999cd511e0ed18cc88

--- packages/modules/sentry/src/tokens.ts
+++ packages/modules/sentry/src/tokens.ts
@@ -1,25 +1,31 @@
-import { createToken } from '@tinkoff/dippy';
+import { createToken, Scope } from '@tinkoff/dippy';
 import type { Sentry, SentryOptions, SentryRequestOptions, Event, EventHint } from './types.h';
 
-export const SENTRY_TOKEN = createToken<Sentry>('sentry');
-export const SENTRY_DSN_TOKEN = createToken<string>('sentryDSNToken');
+export const SENTRY_TOKEN = createToken<Sentry>('sentry', { scope: Scope.SINGLETON });
+export const SENTRY_DSN_TOKEN = createToken<string>('sentryDSNToken', { scope: Scope.SINGLETON });
 export const SENTRY_OPTIONS_TOKEN = createToken<SentryOptions[]>('sentryOptions', {
+  scope: Scope.SINGLETON,
   multi: true,
 });
 export const SENTRY_REQUEST_OPTIONS_TOKEN = createToken<SentryRequestOptions[]>(
   'sentryRequestOptions',
   {
+    scope: Scope.SINGLETON,
     multi: true,
   }
 );
-export const SENTRY_LAZY_LOADING = createToken<boolean>('sentryLazyLoading');
+export const SENTRY_LAZY_LOADING = createToken<boolean>('sentryLazyLoading', {
+  scope: Scope.SINGLETON,
+});
 
 export type ErrorsFilter = (event: Event, hint?: EventHint) => boolean;
 
 export const SENTRY_FILTER_ERRORS = createToken<ErrorsFilter>('SENTRY_FILTER_ERRORS', {
+  scope: Scope.SINGLETON,
   multi: true,
 });
 
 export const SENTRY_SERVER_ENABLE_DEFAULT_HANDLERS = createToken<boolean>(
-  'SENTRY_SERVER_ENABLE_DEFAULT_HANDLERS'
+  'SENTRY_SERVER_ENABLE_DEFAULT_HANDLERS',
+  { scope: Scope.SINGLETON }
 );
