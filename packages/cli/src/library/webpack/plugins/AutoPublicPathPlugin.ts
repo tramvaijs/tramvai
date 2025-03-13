d9e35c7bbd109074e1d12ae75815a414a3bd600d
tramvaijsorg <d.oleg.work@gmail.com>
2025-03-13 12:40:17 +0000

    Release v3.41.55
    Co-authored-by: d.kholstinin <d.kholstinin@tbank.ru>
    GitOrigin-RevId: 1f57741d96c7440d222692ff16201c8ae9182768

---
+++ packages/cli/src/library/webpack/plugins/AutoPublicPathPlugin.ts
@@ -0,0 +1,23 @@
+// Fixes webpack AutoPublicPathPlugin error when fallback load of shared microfrontend chunk
+// this happens because currentScript.src is not defined for an inline script
+export class PatchAutoPublicPathPlugin {
+  apply(compiler) {
+    compiler.hooks.compilation.tap('PatchAutoPublicPathPlugin', (compilation) => {
+      compilation.hooks.runtimeModule.tap('PatchAutoPublicPathPlugin', (module) => {
+        if (module.constructor.name === 'AutoPublicPathRuntimeModule') {
+          const base = module.generate.bind(module);
+
+          // eslint-disable-next-line no-param-reassign
+          module.generate = function (...args) {
+            const result = base(...args);
+
+            return result.replace(
+              'document.currentScript.src',
+              'document.currentScript.src || document.currentScript.dataset.src'
+            );
+          };
+        }
+      });
+    });
+  }
+}
