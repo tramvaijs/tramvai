0a46e2dc1aa6947f9a0d18da9255f42af8226f19
tramvaijsorg <d.oleg.work@gmail.com>
2025-04-21 11:01:13 +0000

    Release v6.0.0
    Co-authored-by: o.drapeza <o.drapeza@tinkoff.ru>
    GitOrigin-RevId: 96ac3c3482c09a1f29ced4c7dbe360ff372f8de4

--- tools/docSite/docusaurus.config.js
+++ tools/docSite/docusaurus.config.js
@@ -118,7 +118,7 @@ const docusaurusConfig = {
       items: [
         process.env.DOCS_VERSIONING_DISABLE !== 'true' && {
           to: '/versions',
-          label: 'v5.x.x',
+          label: 'v6.x.x',
           position: 'right',
         },
         { to: 'docs/get-started/overview', label: 'Docs', position: 'right' },
