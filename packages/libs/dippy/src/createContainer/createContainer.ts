d095ef12d8ec806a261860b2f1ddf1609030d218
tramvaijsorg <d.oleg.work@gmail.com>
2026-03-26 09:11:18 +0000

    Release v5.53.135
    Co-authored-by: d.kholstinin <d.kholstinin@tbank.ru>
    GitOrigin-RevId: be272a9939b9f67377108de53a28e050d0794268

--- packages/libs/dippy/src/createContainer/createContainer.ts
+++
@@ -1,6 +0,0 @@
-import { Container } from '../Container';
-import type { Provider } from '../Provider';
-
-export function createContainer(providers?: Provider[]) {
-  return new Container(providers);
-}
