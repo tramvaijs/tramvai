4270fa75471901f187d4e73197e2b18b47b84173
tramvaijsorg <d.oleg.work@gmail.com>
2026-03-26 09:11:16 +0000

    Release v6.79.40
    Co-authored-by: d.kholstinin <d.kholstinin@tbank.ru>
    GitOrigin-RevId: 1221b4f55bba96431bdf4c67a2c16cca14f10595

--- packages/libs/dippy/src/createError.ts
+++
@@ -1,8 +0,0 @@
-export const createError = (message: string, additional: Record<string, any>) => {
-  const err = new Error(message);
-
-  return Object.assign(err, {
-    ...additional,
-    stack: additional.stack ? `${err.stack}\n---- caused by: ----\n${additional.stack}` : err.stack,
-  });
-};
