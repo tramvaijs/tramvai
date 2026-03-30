d9b68eb6a4ed0ba6c6569da1b08dba9ab689f000
tramvaijsorg <d.oleg.work@gmail.com>
2026-03-30 15:00:22 +0000

    Release v7.14.3
    Co-authored-by: o.drapeza <o.drapeza@tinkoff.ru>
    GitOrigin-RevId: 29601ea56a2797ed35d2da486be9c07cd39527ba

--- packages/modules/common/src/requestManager/RequestManagerModule.ts
+++ packages/modules/common/src/requestManager/RequestManagerModule.ts
@@ -1,5 +1,5 @@
 import { Module, commandLineListTokens, provide } from '@tramvai/core';
-import { REQUEST_MANAGER_TOKEN, CONTEXT_TOKEN } from '@tramvai/tokens-common';
+import { REQUEST_MANAGER_TOKEN, CONTEXT_TOKEN, ENV_MANAGER_TOKEN } from '@tramvai/tokens-common';
 import { setRequest } from './RequestManagerStore';
 import { sharedProviders } from './sharedProviders';
 
@@ -11,12 +11,19 @@ import { sharedProviders } from './sharedProviders';
       multi: true,
       useFactory: ({ context, requestManager }) => {
         return function dehydrateRequestManager() {
+          // do not send IP into browser for SSG, CSR and ISR
+          const disableIpRehydration =
+            requestManager.getHeader('x-tramvai-static-page-revalidate') ||
+            requestManager.getHeader('x-tramvai-prerender');
+
           return context.dispatch(
             setRequest({
               body: requestManager.getBody(),
-              headers: {
-                'x-real-ip': requestManager.getClientIp(),
-              },
+              headers: disableIpRehydration
+                ? {}
+                : {
+                    'x-real-ip': requestManager.getClientIp(),
+                  },
             })
           );
         };
