8d3334601662754af4827262d684a97fe2600c81
tramvaijsorg <d.oleg.work@gmail.com>
2026-04-24 10:26:13 +0000

    Release v6.80.19
    Co-authored-by: Ekaterina Maletskaya <e.maletskaya@tinkoff.ru>
    GitOrigin-RevId: 5893df5c1a4911fd17b79d0bc87e5e0329904081

--- packages/tramvai/test/unit-jest/src/resolver.ts
+++ packages/tramvai/test/unit-jest/src/resolver.ts
@@ -5,6 +5,7 @@ const enhancedResolver = create.sync({
   conditionNames: ['browser', 'require', 'node', 'default'],
   mainFields: ['browser', 'module', 'main'],
   extensions: ['.js', '.jsx', '.mjs', '.ts', '.tsx', '.json', '.node'],
+  aliasFields: ['browser'],
   alias: {
     apps: path.resolve(__dirname, 'apps'),
     packages: path.resolve(__dirname, 'packages'),
