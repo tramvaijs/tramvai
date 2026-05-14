efdab4baf75c987a58d770c1bb12367feac86ab5
tramvaijsorg <d.oleg.work@gmail.com>
2026-05-14 10:45:57 +0000

    Release v7.22.3
    Co-authored-by: o.drapeza <o.drapeza@tinkoff.ru>
    GitOrigin-RevId: 5b0fcfd2d8774c7b6c22f31cd0fbe0a2c7d99015

--- packages/modules/child-app/src/shared/child/extractorProviders.ts
+++ packages/modules/child-app/src/shared/child/extractorProviders.ts
@@ -1,14 +1,26 @@
 import { Scope, provide } from '@tramvai/core';
-import { CHILD_APP_INTERNAL_CHUNK_EXTRACTOR } from '@tramvai/tokens-child-app';
+import {
+  CHILD_APP_INTERNAL_CHUNK_EXTRACTOR,
+  CHILD_APP_INTERNAL_CONFIG_TOKEN,
+  CHILD_APP_LOADER_TOKEN,
+} from '@tramvai/tokens-child-app';
 import { ChunkExtractor } from '@loadable/server';
+import { Container } from '@tinkoff/dippy';
 import type { LoadableStats } from '../webpack/moduleFederation';
 
-export const extractorProviders = (loadableStats?: LoadableStats) => [
+export const extractorProviders = (appDi: Container) => [
   provide({
     provide: CHILD_APP_INTERNAL_CHUNK_EXTRACTOR,
     scope: Scope.REQUEST,
-    useFactory: () => {
-      return new ChunkExtractor({ stats: loadableStats ?? {}, entrypoints: [] });
+    useFactory: ({ config }) => {
+      const loader = appDi.get(CHILD_APP_LOADER_TOKEN);
+      const statsLoadable: LoadableStats | undefined =
+        'getLoadableStats' in loader ? (loader as any).getLoadableStats(config) : undefined;
+
+      return new ChunkExtractor({ stats: statsLoadable ?? {}, entrypoints: [] });
+    },
+    deps: {
+      config: CHILD_APP_INTERNAL_CONFIG_TOKEN,
     },
   }),
 ];
